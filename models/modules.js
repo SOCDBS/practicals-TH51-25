55689a5f4408a81a4ed5b47ab95ee57ce01f983e
Wei Han <th51.25@ichat.sp.edu.sg>
2026-07-21 13:28:06 +0800

    Completed practical 3 of ORM

--- models/modules.js
+++ models/modules.js
@@ -4,10 +4,89 @@ const prisma = new PrismaClient();
 module.exports.create = function create(code, name, credit) {
     return prisma.module.create({
         //TODO: Add data
+        data: {
+            modCode: code,
+            modName: name,
+            creditUnit: Number(credit)
+        }
     }).then(function (module) {
         //TODO: Return module
+        return module;
     }).catch(function (error) {
         // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#p2002
         // TODO: Handle Prisma Error, throw a new error if module already exists
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                console.log("There is a unique constraint violation, a new module cannot be created with this code")
+                console.log("Error:", error)
+            }
+        }
+        throw error;
     });
-};
+}
+
+module.exports.updateByCode = function updateByCode(code, credit) {
+    return prisma.module.update({
+        where: {
+            modCode: code
+        },
+        data: {
+            creditUnit: Number(credit)
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                console.log("Error:", error)
+            }
+        }
+        throw error;
+    });
+}
+
+module.exports.deleteByCode = function deleteByCode(code) {
+    return prisma.module.delete({
+        where: {
+            modCode: code
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                console.log("Error:", error);
+            }
+        }
+        throw error;
+    });
+}
+
+module.exports.retrieveAll = function retrieveAll() {
+    return prisma.module.findMany({
+
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error) {
+            console.log("Error:", error);
+        }
+    });
+}
+
+module.exports.retrieveByCode = function retrieveByCode(code) {
+    return prisma.module.findUnique({
+        where: {
+            modCode: code
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                console.log("Error:", error);
+            }
+        }
+        throw error;
+    });
+}
